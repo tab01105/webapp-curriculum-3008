20ef446744f07b3724b7cb0edd59b3199cb16b8b
tab01105 <tab01105@gmail.com>
2018-02-21 14:49:34 +0900

    初級MessageCountActorAppの実装

--- src/main/scala/MessageCountActorApp.scala
+++ src/main/scala/MessageCountActorApp.scala
@@ -1,3 +1,28 @@
-object MessageCountActorApp extends App {
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.event.Logging
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class MyActor extends Actor {
+  var counter = 0
+
+  def receive = {
+    case _ => {
+      counter += 1
+      println(counter)
+    }
+  }
+}
+
+object MessageCountActorApp extends App {
+  val system = ActorSystem("actorStudy")
+
+  val myActor = system.actorOf(Props[MyActor], "myActor")
+
+  for (i <- 1 to 10000)
+    myActor ! "send"
+
+//  system.terminate()
+  Await.ready(system.terminate(), Duration.Inf)
 }
